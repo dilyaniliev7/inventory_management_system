d21f05c6ceec270f750228a61aedb610320fa9cb
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-09-21 18:04:13 +0300

    displayed items in template, login required added

--- inventory/views.py
+++ inventory/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, View
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, View, CreateView
 from django.contrib.auth import authenticate, login
-from .forms import UserRegisterForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import UserRegisterForm, InventoryItemForm
+from .models import InventoryItem, Category
+
 
 class Index(TemplateView):
     template_name = 'inventory/index.html'
 
 
-class Dashboard(View):
+class Dashboard(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, 'inventory/dashboard.html')
+        items = InventoryItem.objects.filter(user=self.request.user.id).order_by('id')
+
+        return render(request, 'inventory/dashboard.html', {'items': items})
 
 class SignUpView(View):
     def get(self, request):
-        form = UserRegisterFom()
+        form = UserRegisterForm()
         return render(request, 'inventory/signup.html', {'form': form})
 
     def post(self, request):
@@ -29,3 +35,19 @@ class SignUpView(View):
             login(request, user)
             return redirect('index')
         return render(request, 'inventory/signup.html', {'form': form})
+
+
+class AddItem(LoginRequiredMixin, CreateView):
+    model = InventoryItem
+    form_class = InventoryItemForm
+    template_name = 'inventory/item_form.html'
+    success_url = reverse_lazy('dashboard')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
