c7cd90237214b7b7718c36b49b760f8618b469e3
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-09-23 19:31:19 +0300

    edit, delete item / quantity alerts

--- inventory/views.py
+++ inventory/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, View, CreateView
+from django.views.generic import TemplateView, View, CreateView, UpdateView, DeleteView
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserRegisterForm, InventoryItemForm
 from .models import InventoryItem, Category
+from django.conf import settings
+from django.contrib import messages
 
 
 class Index(TemplateView):
@@ -15,7 +17,23 @@ class Dashboard(LoginRequiredMixin, View):
     def get(self, request):
         items = InventoryItem.objects.filter(user=self.request.user.id).order_by('id')
 
-        return render(request, 'inventory/dashboard.html', {'items': items})
+        low_inventory = InventoryItem.objects.filter(
+            user=self.request.user.id,
+            quantity__lte=settings.LOW_QUANTITY
+        )
+
+        if low_inventory.count() > 0:
+            if low_inventory.count() > 1:
+                messages.error(request, f'{low_inventory.count()} items have low inventory')
+            else:
+                messages.error(request, f'{low_inventory.count()} items have low inventory')
+
+        low_inventory_ids = InventoryItem.objects.filter(
+            user=self.request.user.id,
+            quantity__lte=settings.LOW_QUANTITY
+        ).values_list('id', flat=True)
+
+        return render(request, 'inventory/dashboard.html', {'items': items, 'low_inventory_ids': low_inventory_ids})
 
 class SignUpView(View):
     def get(self, request):
@@ -51,3 +69,17 @@ class AddItem(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class EditItem(LoginRequiredMixin, UpdateView):
+    model = InventoryItem
+    form_class = InventoryItemForm
+    template_name = 'inventory/item_form.html'
+    success_url = reverse_lazy('dashboard')
+
+
+class DeleteItem(LoginRequiredMixin, DeleteView):
+    model = InventoryItem
+    template_name = 'inventory/delete_item.html'
+    success_url = reverse_lazy('dashboard')
+    context_object_name = 'item'
